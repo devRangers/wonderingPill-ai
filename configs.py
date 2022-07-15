502e3b933a1ed88daef7bb6cfde5722d29164a6c
KimByeolhee <qufgml0216@naver.com>
2022-07-15 14:25:28 +0900

    [Add] initial setting of configs

--- configs.py
+++ configs.py
@@ -1,4 +1,7 @@
 import argparse
 
 mn_parser = argparse.ArgumentParser(description="mobilenet")
-mn_parser.add_argument("")
+
+mn_parser.add_argument("--batch_size", type=int, default=64)
+
+mn_config = mn_parser.parse_args()
---
+++ train.py
@@ -0,0 +1,9 @@
+from pill_classification.data_loader import get_train_valid_test, DataLoader
+from configs import mn_config
+
+def main(config):
+    pass
+
+if __name__ == "__main__":
+    config = mn_config
+    main(config)
