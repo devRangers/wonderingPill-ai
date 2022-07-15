e8bc95d6ba9950f5f09599b184bb3966187c8ed7
KimByeolhee <qufgml0216@naver.com>
2022-07-15 15:09:23 +0900

    [Add] Load DataLoader by running train.py

--- pill_classification/data_loader.py
+++ pill_classification/data_loader.py
@@ -2,18 +2,20 @@ import sklearn
 import cv2
 import numpy as np
 import pandas as pd
+import tensorflow
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import Sequence
 
 
-def get_train_valid_test(data_df, valid_size=0.1, random_state=216):
-  data_path = data_df['path'].values
-  data_label = pd.get_dummies(data_df['label']).values
+def get_train_valid_test(df_path, valid_size=0.1, random_state=216):
+    data_df = pd.read_csv(df_path, encoding="euc-kr")
+    data_path = data_df['path'].values
+    data_label = pd.get_dummies(data_df['label']).values
 
-  X_train, X_test, y_train, y_test = train_test_split(data_path, data_label, test_size=valid_size, stratify=data_label, random_state=216)
-  X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=(valid_size / (1-valid_size)), stratify=y_train, random_state=216)
+    X_train, X_test, y_train, y_test = train_test_split(data_path, data_label, test_size=valid_size, stratify=data_label, random_state=random_state)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=(valid_size / (1-valid_size)), stratify=y_train, random_state=random_state)
 
-  return X_train, X_valid, X_test, y_train, y_valid, y_test
+    return X_train, X_valid, X_test, y_train, y_valid, y_test
 
 
 class DataLoader(Sequence):
--- train.py
+++ train.py
@@ -1,8 +1,44 @@
 from pill_classification.data_loader import get_train_valid_test, DataLoader
+from tensorflow.keras.applications.xception import preprocess_input
+import albumentations as A
 from configs import mn_config
 
+DF_PATH = "./data/pills_data.preprocess.csv"
+IMAGE_SIZE = 224
+
 def main(config):
-    pass
+    X_train, X_valid, X_test, y_train, y_valid, y_test = get_train_valid_test(df_path=DF_PATH, valid_size=0.1)
+
+    augmentor_Flip = A.Compose([
+         A.HorizontalFlip(p=0.5)
+    ])
+
+    train_loader = DataLoader(X_train, y_train,
+                                image_size=IMAGE_SIZE,
+                                batch_size=config.batch_size,
+                                augmentor=augmentor_Flip,
+                                shuffle=True,
+                                pre_func = preprocess_input)
+
+    valid_loader = DataLoader(X_valid, y_valid,
+                                image_size=IMAGE_SIZE,
+                                batch_size=config.batch_size,
+                                augmentor=None,
+                                shuffle=False,
+                                pre_func=preprocess_input)
+
+    test_loader = DataLoader(X_test, y_test,
+                            image_size=IMAGE_SIZE,
+                            batch_size=config.batch_size,
+                            augmentor=None,
+                            shuffle=False,
+                            pre_func=preprocess_input)
+    
+    train_batch = next(iter(train_loader))[0]
+    valid_batch = next(iter(valid_loader))[0]
+    test_batch = next(iter(test_loader))[0]
+
+    print(train_batch.shape, valid_batch.shape, test_batch.shape)
 
 if __name__ == "__main__":
     config = mn_config
