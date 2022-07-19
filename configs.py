cf0d16b0cebd447f656dd5ed1ebf9523d04fe55a
KimByeolhee <qufgml0216@naver.com>
2022-07-19 14:57:07 +0900

    [Fix] remove overlapping batch_size config

--- configs.py
+++ configs.py
@@ -7,6 +7,5 @@ mn_parser.add_argument("--gpu_id", type=int, default=-1)
 mn_parser.add_argument("--n_classes", type=int)
 mn_parser.add_argument("--verbose", type=int, default=1)
 mn_parser.add_argument("--n_epochs", type=int, default=10)
-mn_parser.add_argument("--batch_size", type=int, default=64)
 
 mn_config = mn_parser.parse_args()
